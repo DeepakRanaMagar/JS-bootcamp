1d6566b8c702ec09380979c1fde6d1cd3bd04f2a
Sibindra <timalsinasibindra@gmail.com>
2022-11-20 14:04:04 +0545

    added new container and centered it

--- loginJS/script.js
+++ loginJS/script.js
@@ -1,24 +1,23 @@
-function login(){
-    console.log("You clicked login button")
+function login() {
+    console.log("You clicked login button");
 }
 
-
-
-function showPassword(){
+function showPassword() {
     const password = document.querySelector("#password");
 
-        togglePassword.addEventListener("click", function () {
-            // toggle the type attribute
-            const type = password.getAttribute("type") === "password" ? "text" : "password";
-            password.setAttribute("type", type);
-            
-            // toggle the icon
-            this.classList.toggle("bi-eye");
-        });
+    togglePassword.addEventListener("click", function () {
+        // toggle the type attribute
+        const type =
+            password.getAttribute("type") === "password" ? "text" : "password";
+        password.setAttribute("type", type);
 
-        // prevent form submit
-        const form = document.querySelector("form");
-        form.addEventListener('submit', function (e) {
-            e.preventDefault();
-        });
+        // toggle the icon
+        this.classList.toggle("bi-eye");
+    });
+
+    // prevent form submit
+    const form = document.querySelector("form");
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+    });
 }
