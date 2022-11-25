50a34b920706dcbe9d5c7f7a58d070f8dc4a2088
DeepakRanaMagar <dpk.me.123@gmail.com>
2022-11-25 20:57:42 +0545

    Week2

--- simpleCalculatorJS/script.js
+++ simpleCalculatorJS/script.js
@@ -1,6 +1,25 @@
-const seven = document.getElementById(seven);
+// variable for integers
+const btnNum = document.querySelectorAll('.btn-num')
+// variable for operators
+const operators = document.querySelectorAll('.operator')
+// variable for clear
+const clear = document.querySelectorAll('.clear')
+// variable for display
+const span= document.getElementById('display-section')
 
-function displayValue(){
-    console.log("clicked");
-    display-Selection.innerhtml(seven);
+//function to display
+function display(number){
+    span.innerHTML = span.innerHTML.concat(number.innerHTML)
 }
+
+for (let i = 0; i<btnNum.length; i++){
+
+    btnNum[i].onclick=()=>{
+    
+        display(btnNum[i]);
+    
+    }
+}
+
+//function to perform operation
+
