5fdd12b1615a68c58fc35e068ffef23cdc0c5a38
DeepakRanaMagar <dpk.me.123@gmail.com>
2022-11-26 10:08:00 +0545

    Week 2

--- simpleCalculatorJS/script.js
+++ simpleCalculatorJS/script.js
@@ -3,23 +3,46 @@ const btnNum = document.querySelectorAll('.btn-num')
 // variable for operators
 const operators = document.querySelectorAll('.operator')
 // variable for clear
-const clear = document.querySelectorAll('.clear')
+const clearAll = document.getElementById('ce')
+const clearSingle = document.querySelectorAll('.clear-single')
+const empty = ""
+
+//to append values to variable 
+var currentNum = ""
+var perviousNum = ""
+
+function appendValue(){
+    currentNum = span.innerHTML
+    // console.log(currentNum)
+}
+
 // variable for display
 const span= document.getElementById('display-section')
+
+
+
 
 //function to display
 function display(number){
     span.innerHTML = span.innerHTML.concat(number.innerHTML)
+    
 }
-
 for (let i = 0; i<btnNum.length; i++){
 
     btnNum[i].onclick=()=>{
-    
         display(btnNum[i]);
-    
+        appendValue()
     }
 }
 
-//function to perform operation
+
+
+
+//clear-all garna 
+function clear() {
+    span.innerHTML=""
+}
+clearAll.onclick=()=>{
+    clear()
+}
 
